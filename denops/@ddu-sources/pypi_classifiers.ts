40641f8ce6d91654053e9c171980e95ccd428aa5
4513ECHO <mail@4513echo.dev>
2023-08-28 18:41:13 +0900

    fix: Return new stream always

--- denops/@ddu-sources/pypi_classifiers.ts
+++ denops/@ddu-sources/pypi_classifiers.ts
@@ -2,16 +2,16 @@ import type { ActionData } from "https://deno.land/x/ddu_kind_word@v0.1.2/word.t
 import {
   BaseSource,
   type OnInitArguments,
-} from "https://deno.land/x/ddu_vim@v3.4.3/base/source.ts";
-import type { Item } from "https://deno.land/x/ddu_vim@v3.4.3/types.ts";
-import { TextLineStream } from "https://deno.land/std@0.194.0/streams/text_line_stream.ts";
+} from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
+import type { Item } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { TextLineStream } from "https://deno.land/std@0.200.0/streams/text_line_stream.ts";
 import { ChunkedStream } from "https://deno.land/x/chunked_stream@0.1.2/mod.ts";
 
 type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params, ActionData> {
   override kind = "word";
-  #stream: ReadableStream<Item<ActionData>[]> = new ReadableStream();
+  #stream?: () => ReadableStream<Item<ActionData>[]>;
 
   override async onInit(args: OnInitArguments<Params>): Promise<void> {
     const response = await fetch(
@@ -25,23 +25,24 @@ export class Source extends BaseSource<Params, ActionData> {
       );
       return;
     }
-    this.#stream = response.body!
-      .pipeThrough(new TextDecoderStream())
-      .pipeThrough(new TextLineStream())
-      .pipeThrough(
-        new TransformStream<string, Item<ActionData>>({
-          transform: (chunk, controller) =>
-            controller.enqueue({
-              word: chunk,
-              action: { text: chunk },
-            }),
-        }),
-      )
-      .pipeThrough(new ChunkedStream({ chunkSize: 100 }));
+    this.#stream = () =>
+      response.clone().body!
+        .pipeThrough(new TextDecoderStream())
+        .pipeThrough(new TextLineStream())
+        .pipeThrough(
+          new TransformStream<string, Item<ActionData>>({
+            transform: (chunk, controller) =>
+              controller.enqueue({
+                word: chunk,
+                action: { text: chunk },
+              }),
+          }),
+        )
+        .pipeThrough(new ChunkedStream({ chunkSize: 100 }));
   }
 
   override gather(_args: unknown): ReadableStream<Item<ActionData>[]> {
-    return this.#stream;
+    return this.#stream!();
   }
 
   override params(): Params {
